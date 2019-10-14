344517067da233fafc4a7d0a539fead84ead9d26
davidsleeer <102831215@student.swin.edu.au>
2019-10-14 11:01:52 +1100

    Task 3 Completed

--- SINGLE.sql
+++ SINGLE.sql
@@ -69,3 +69,61 @@ CREATE TABLE Booking (
 ,CHECK (EventYear>999 AND EventYear<10000)
 ,CHECK (Payment > 0)
 );
+
+INSERT INTO Tour (TourName, Description)
+VALUES ('North', 'Tour of wineries and outlets of the Bedigo and Castlemaine region');
+INSERT INTO Tour (TourName, Description)
+VALUES ('South', 'Tour of wineries and outlets of Mornington Penisula');
+INSERT INTO Tour (TourName, Description)
+VALUES ('West', 'Tour of wineries and outlets of the Geelong and Otways region');
+
+INSERT INTO Client (ClientID, Surname, GIvenName, Gender)
+VALUES (1, 'Price', 'Taylor','M');
+INSERT INTO Client (ClientID, Surname, GIvenName, Gender)
+VALUES (2, 'Bamble', 'Ellyse','M');
+INSERT INTO Client (ClientID, Surname, GIvenName, Gender)
+VALUES (3, 'Tan', 'Tilly','F');
+INSERT INTO Client (ClientID, Surname, GIvenName, Gender)
+VALUES (102831215, 'Li', 'Dawei','M');
+
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+VALUES ('NORTH', 'Jan', 9, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+VALUES ('NORTH', 'Feb', 13, 2016, 225);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+VALUES ('SOUTH', 'Jan', 9, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+VALUES ('SOUTH', 'Jan', 16, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+VALUES ('West', 'Jan', 29, 2016, 225);
+
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (1, 'NORTH', 'Jan', 9, 2016, 200, '10-Dec-2015');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (2, 'NORTH', 'Jan', 9, 2016, 200, '16-Dec-2015');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (1, 'NORTH', 'Feb', 13, 2016, 225, '08-Jan-2016');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (2, 'NORTH', 'Feb', 13, 2016, 125, '14-Jan-2016');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (3, 'NORTH', 'Feb', 13, 2016, 225, '03-Feb-2016');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (1, 'SOUTH', 'Jan', 9, 2016, 200, '10-Dec-2015');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (2, 'SOUTH', 'Jan', 16, 2016, 200, '18-Dec-2015');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (3, 'SOUTH', 'Jan', 16, 2016, 200, '09-Jan-2016');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (2, 'WEST', 'Jan', 29, 2016, 225, '17-Dec-2015');
+INSERT INTO Booking (ClientID, TourName, EventMonth, EventDay, EventYear,
+Payment, Datebooked)
+VALUES (3, 'WEST', 'Jan', 29, 2016, 200, '18-Dec-2015');
